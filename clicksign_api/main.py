0ad58df165daff402aae160c775d12f1da068a51
vinicius.gomes <viniciusgueimis@gmail.com>
2026-05-14 16:51:18 -0300

    Função adicionar signatários

--- clicksign_api/main.py
+++ clicksign_api/main.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 from pathlib import Path
+import json
 #---------------- Variáveis Globais ----------------#
 url_envelope = "https://sandbox.clicksign.com/api/v3/envelopes"
 
@@ -8,7 +9,7 @@ url_envelope = "https://sandbox.clicksign.com/api/v3/envelopes"
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/vnd.api+json",
-    "Authorization": "a49cd34f-17b1-414a-b622-ea8c54829e5c"
+    "Authorization": "cac9712a-7fcc-41ea-bb71-7f7cf2bb214a"
 }
 
 
@@ -19,48 +20,28 @@ caminho = basedir / "Documento" / "documento.pdf"
 #criar envelopoes
 def criar_envelope():
     """"Essa função vai criar o envelope (atribuir valores variáveis)"""
+
     payload = "{\"data\":{\"type\":\"envelopes\",\"attributes\":{\"name\":\"Envelope de Teste\",\"locale\":\"pt-BR\",\"auto_close\":true,\"remind_interval\":3,\"block_after_refusal\":false}}}"
+
     response = requests.post(url_envelope, data=payload, headers=headers)
+
     if response.status_code == 201:
         print("Envelope criado")
+        data = response.json()
+        return data["data"]["id"]
     elif response.status_code == 400:
         print("Erro ao criar o envelope")
+    return None
 
-
-def obter_id_envelope():
-    """Função necessária para guardar o ID do envelope"""
-    #obtém os dados do envelope
-    response = requests.get(url_envelope, headers=headers)
-    #guarda esses dados em json
-    envelopes = response.json()
-    print(envelopes)
-
-    #guarda a chave data
-    data = envelopes["data"]
-
-    if data and len(data) > 0:
-        envelope_id = data[0]["id"]
-        return envelope_id
-    else:
-        return None
-
-
-def ativar_envelope(envelope_id):
+def ativar_envelope():
     """"Função para Ativar o envelope. Só funciona quando passar por todas as etapas"""
-    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{envelope_id}/activate"
+    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{id_envelope}/activate"
     response = requests.post(url, headers=headers)
     if response.status_code == 202:
         print("Envelope ativo")
     else:
         print("Erro ao ativar o envelope")
-
-    # url_ativar = f"https://sandbox.clicksign.com/api/v3/envelopes/{envelope_id}"
-    # payload = "{\"data\":{\"type\":\"envelopes\",\"attributes\":{\"status\":\"running\",\"name\":\"autodeinfracao\",\"locale\":\"pt-BR\",\"auto_close\":true},\"id\":123}}"
-    # response = requests.post(url_ativar, data = payload, headers = headers)
-    # if response.status_code == 200:
-    #     print("Envelope ativo e Rodando")
-    # else :
-    #     print("Erro ao ativar o rodando")
+        print(response.status_code)
 
 def gerar_base64_pdf():
     """"Conversão do arquivo da pasta 'Documentos' para base64"""
@@ -76,10 +57,11 @@ def gerar_base64_pdf():
 
 
 
-def criar_documento(envelope_id):
+
+def criar_documento():
     """Cria um documento dentro do envelope"""
 
-    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{envelope_id}/documents"
+    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{id_envelope}/documents"
     conteudo = gerar_base64_pdf()
     payload = {
         "data": {
@@ -91,15 +73,18 @@ def criar_documento(envelope_id):
         }
     }
 
-    response = requests.post(url, json=payload, headers=headers)
+    payload = json.dumps(payload)
+    response = requests.post(url, data=payload, headers=headers)
 
     if response.status_code == 201:
         print("Documento criado no envelope")
+        data = response.json()
+        return data["data"]["id"]
     else:
         print("Erro ao criar o documento")
         print("Status:", response.status_code)
         print("Resposta:", response.text)
-
+        return None
 
 def listar_signatarios():
     """"Lista os signatários presentes no envelope"""
@@ -110,11 +95,116 @@ def listar_signatarios():
     elif response.status_code == 404:
         print("Erro ao listar os envelopes")
 
+def adicionar_signatario():
+    """Função para adcionar um signatario no envelope"""
+    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{id_envelope}/signers"
+
+    signatario = "Vinicius Santos"
+    email_signatario = "viniciusgueimis@gmail.com"
+    payload = {
+    "data": {
+        "type": "signers",
+        "attributes": {
+            "has_documentation": False,
+            "refusable": False,
+            "group": 1,
+            "location_required_enabled": False,
+            "communicate_events": {
+                "signature_request": "none",
+                "signature_reminder": "none",
+                "document_signed": "email"
+            },
+            "name": signatario,
+            "email": email_signatario,
+        }
+    }
+}
+    payload = json.dumps(payload)
+    response = requests.post(url, data=payload, headers=headers)
+
+    if response.status_code ==  201:
+        data = response.json()
+        nome = data["data"]["attributes"]["name"]
+        email = data["data"]["attributes"]["email"]
+        id= data["data"]["id"]
+        print(f"Signaário {nome} Adicionado no envelope {id_envelope} com o email {email}")
+        print(response.text)
+        return id
+    else:
+        print("Erro ao adicionar o signatario no envelope")
+        print("Status:", response.status_code)
+        return None
+
+
+def criar_requisito():
+    """Essa função cria um requisito para o envelope, vinculando um documento a um signatario"""
+
+    url = f"https://sandbox.clicksign.com/api/v3/envelopes/{id_envelope}/requirements"
+
+    payload_qualifica = {
+        "data": {
+            "type": "requirements",
+            "attributes": {
+                "action": "agree",
+                "role": "sign"
+            },
+            "relationships": {
+                "document": {
+                    "data": {
+                        "type": "documents",
+                        "id": id_documento
+                    }
+                },
+                "signer": {
+                    "data": {
+                        "type": "signers",
+                        "id": id_signatario
+                    }
+                }
+            }
+        }
+    }
+    payload_qualifica = json.dumps(payload_qualifica)
+    response = requests.post(url, data=payload_qualifica, headers=headers)
+
+    payload_autentica = {
+    "data": {
+        "type": "requirements",
+        "attributes": {
+            "action": "provide_evidence",
+            "auth": "email"
+        },
+        "relationships": {
+            "document": {
+                "data": {
+                    "type": "documents",
+                    "id": id_documento
+                }
+            },
+            "signer": {
+                "data": {
+                    "type": "signers",
+                    "id": id_signatario
+                }
+            }
+        }
+    }
+}
+    payload_autentica = json.dumps(payload_autentica)
+    request2 = requests.post(url, data=payload_autentica, headers=headers)
+
+    if response.status_code == 201:
+        print("Requisito de qualificação criado no envelope")
+    if request2.status_code == 201:
+        print("requisito de autenticacao no envelope")
+
 
 
 if __name__ == "__main__":
-    id_envelope = obter_id_envelope()
-    criar_documento(id_envelope)
-    criar_envelope()
-    criar_documento(id_envelope)
-    ativar_envelope(obter_id_envelope())
+    id_envelope = criar_envelope()
+    id_documento = criar_documento()
+    print("id Documento: ", id_documento)
+    id_signatario = adicionar_signatario()
+    print("id Signatario",id_signatario)
+    criar_requisito()
+    ativar_envelope()
