df7bf067e7978a5dd5f469b472c5a35bb3cfa38b
vinicius.gomes <viniciusgueimis@gmail.com>
2026-06-02 15:31:52 -0300

    Função adicionar signatários

--- clicksign_api/services/requisitos.py
+++ clicksign_api/services/requisitos.py
@@ -1,16 +1,17 @@
 import requests
 from clicksign_api.config import BASE_URL, HEADERS
-def criar_requisitos(envelope_id, document_id, signer_id):
+
+def criar_requisitos(envelope_id, document_id, signer_id, role = "sign", auth = "email"):
     """Essa função cria um requisito para o envelope, vinculando um documento a um signatario"""
 
     url = f"{BASE_URL}/envelopes/{envelope_id}/requirements"
 
-    payload_qualifica = {
+    payload_assinatura = {
         "data": {
             "type": "requirements",
             "attributes": {
                 "action": "agree",
-                "role": "sign"
+                "role": role
             },
             "relationships": {
                 "document": {"data": {"type": "documents", "id": document_id}},
@@ -19,34 +20,42 @@ def criar_requisitos(envelope_id, document_id, signer_id):
         }
     }
 
-    response = requests.post(url, json=payload_qualifica, headers=HEADERS)
+    response_assinatura = requests.post(url, json=payload_assinatura, headers=HEADERS)
 
-    payload_autentica = {
-    "data": {
-        "type": "requirements",
-        "attributes": {
-            "action": "provide_evidence",
-            "auth": "email"
-        },
-        "relationships": {
-            "document": {
-                "data": {
-                    "type": "documents",
-                    "id": document_id
-                }
+    if response_assinatura.status_code != 201:
+        print("Erro ao criar requisito de assinatura")
+        print("Status:", response_assinatura.status_code)
+        print("Resposta:", response_assinatura.text)
+        return None, None
+        requirement_assinatura_id = response_assinatura.json()["data"]["id"]
+
+    payload_autenticacao = {
+        "data": {
+            "type": "requirements",
+            "attributes": {
+                "action": "provide_evidence",
+                "auth": auth
             },
-            "signer": {
-                "data": {
-                    "type": "signers",
-                    "id": signer_id
+            "relationships": {
+                "document": {
+                    "data": {
+                        "type": "documents",
+                        "id": document_id
+                    }
+                },
+                "signer": {
+                    "data": {
+                        "type": "signers",
+                        "id": signer_id
+                    }
                 }
             }
         }
     }
-}
-    request2 = requests.post(url, json=payload_autentica, headers=HEADERS)
+
+    response_autenticacao = requests.post(url, json=payload_autenticacao, headers=HEADERS)
 
     if response.status_code == 201:
         print("Requisito de qualificação criado no envelope")
-    if request2.status_code == 201:
+    if response_autenticacao.status_code == 201:
         print("requisito de autenticacao no envelope")
